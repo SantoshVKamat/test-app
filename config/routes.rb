6e6d39cef467267193ecc34b5a8a163e16fe6534
Santosh Kamat <kamatsantosh7194@gmail.com>
2020-08-24 13:23:07 +0530

    Initial code

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+resources :photos do
+  member do
+    get 'preview'
+  end
+end
+
+resources :photos do
+  collection do
+    get 'search'
+  end
+end
+
+
+resources :comments do
+  get 'preview', on: :new
+end
+
+end
